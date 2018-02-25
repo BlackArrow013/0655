573183ebe39e997f72a0bb80c279927e5c6ac6ad
Jorge Jaular Lasaga <jordijaulas13@gmail.com>
2018-02-25 18:08:13 +0100

    Archivos de datos añadidos, y ahora la clase AnalizadorAccesosAServidor contiene un método que lee los archivos.

--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -1,33 +1,34 @@
-
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 /**
- * Write a description of class AnalizadorAccesosAServidor here.
+ * Esta clase analiza los accesos al servidor. Se compondrá de dos métodos, uno
+ * que importará un documento de texto con los accesos al servidor y otro que 
+ * obtendrá qué hora tiene más accesos.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Jorge Jaular Lasaga) 
+ * @version (25/02/2018)
  */
 public class AnalizadorAccesosAServidor
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class AnalizadorAccesosAServidor
-     */
-    public AnalizadorAccesosAServidor()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
     /**
      * An example of a method - replace this comment with your own
      * 
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public int sampleMethod(int y)
+    public void analizarArchivoDeLog(String nombreArchivo)
     {
-        // put your code here
-        return x + y;
+        try {
+            File archivo = new File(nombreArchivo);
+            Scanner sc = new Scanner(archivo);            
+            while (sc.hasNextLine()) {  
+                System.out.println(sc.nextLine());
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
